c110e42d51fc792e9a907ab7b1e51df9b532e569
Laszlo Norbert <laszlo.norbert@kadeno.ro>
2024-06-07 09:28:11 +0300

    enable cors and global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { config } from "@shared/config/config";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "*",
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(config.get("server.port") || 8000);
 }
 

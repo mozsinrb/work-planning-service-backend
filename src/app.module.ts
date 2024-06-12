0dd73ec5911260c13cda865257e9e8259cbd9f17
Laszlo Norbert <laszlo.norbert@kadeno.ro>
2024-06-12 09:16:14 +0300

    add current worker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 
 import { WorkerModule } from "./worker/worker.module";
 import { SharedModule } from "./shared/shared.module";
-import { ShiftModule } from './shift/shift.module';
+import { ShiftModule } from "./shift/shift.module";
 
 @Module({
   imports: [WorkerModule, SharedModule, ShiftModule],
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "*",
+    origin: "http://localhost:4200",
+    credentials: true,
   });
 
   app.useGlobalPipes(
